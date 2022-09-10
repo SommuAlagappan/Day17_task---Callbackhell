ed471ae426a345f2acff98ce082ed32708f39e43
SommuAlagappan <107310959+SommuAlagappan@users.noreply.github.com>
2022-09-10 19:31:24 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,44 @@
+setTimeout(() => {
+    document.getElementById("p").innerHTML = 10;
+    setTimeout(() => {
+        document.getElementById("p").innerHTML = 9;
+        setTimeout(() => {
+            document.getElementById("p").innerHTML = 8;
+            setTimeout(() => {
+                document.getElementById("p").innerHTML = 7;
+                setTimeout(() => {
+                    document.getElementById("p").innerHTML = 6;
+                    setTimeout(() => {
+                        document.getElementById("p").innerHTML = 5;
+                        setTimeout(() => {
+                            document.getElementById("p").innerHTML = 4;
+                            setTimeout(() => {
+                                document.getElementById("p").innerHTML = 3;
+                                setTimeout(() => {
+                                    document.getElementById("p").innerHTML = 2;
+                                    setTimeout(() => {
+                                        document.getElementById("p").innerHTML = 1;
+                                        setTimeout(() => {
+                                            document.getElementById("p").innerHTML = "Happy Birthday";
+                                        
+                                        }, 1000);
+                                    
+                                    }, 1000);
+                                
+                                }, 1000);
+                            
+                            }, 1000);
+                        
+                        }, 1000);
+                    
+                    }, 1000);
+                
+                }, 1000);
+            
+            }, 1000);
+        
+        }, 1000);
+    
+    }, 1000);
+
+}, 1000);
